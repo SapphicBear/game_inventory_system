a536554b98e3e9c5a7d445d8a299a9de0990d322
SapphicBear <sleepyscreenart@gmail.com>
2026-03-26 19:07:39 +0100

    Create tables for the database and make relations

--- db/populatedb.js
+++ db/populatedb.js
@@ -4,11 +4,24 @@ const { argv } = require("node:process");
 const { Client } = require("pg");
 
 const SQL = `
+CREATE TABLE IF NOT EXISTS game_consoles (
+    console_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    company VARCHAR(100),
+    release_year INTERVAL YEAR NOT NULL
+);
+CREATE TABLE IF NOT EXISTS game_studios (
+    studio_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(200),
+    year INTERVAL YEAR NOT NULL
+);
 CREATE TABLE IF NOT EXISTS games (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR (255),
-    release_date DATE,
-    creator VARCHAR (255)
+    name VARCHAR(255),
+    studio_id INTEGER NOT NULL REFERENCES game_studios(studio_id),
+    console_id  INTEGER NOT NULL REFERENCES game_consoles(console_id),
+    genre VARCHAR(255),
+    release_year INTERVAL YEAR NOT NULL
 );
 `;
 
@@ -23,4 +36,4 @@ async function main(arg) {
     console.log("done!")
 }
 
-// main(argv[2]);
+main(argv[2]);
