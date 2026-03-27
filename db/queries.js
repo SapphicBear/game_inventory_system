6f3e0da314c043ec7c41f3d4551410691a33db28
SapphicBear <sleepyscreenart@gmail.com>
2026-03-27 17:50:54 +0100

    Add some starting queries for the database
    Add some default queries giving all data in all tables and making them
    human readable in the case of the games table.

--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,35 @@
 const pool = require("./pool");
 
 // get all items
+async function getAllGames() {
+    const query = `
+    SELECT games.name, game_studios.name, genre, games.release_year, game_consoles.name FROM games
+        JOIN game_consoles 
+        ON (games.console_id = game_consoles.console_id)
+        JOIN game_studios
+        ON (games.studio_id = game_studios.studio_id);
+    `
+    const { rows } = await pool.query(query);
+    return rows;
+}
+
+async function getAllConsoles() {
+    const { rows } = await pool.query("SELECT name, company, release_year FROM game_consoles;");
+    return rows;
+}
+async function getAllStudios() {
+    const { rows } = await pool.query("SELECT name, year FROM game_studios;");
+    return rows;
+}
 
 // get all items of one category
 
 // delete items (DELETE)
 
 // add items (POST)
+
+module.exports = {
+    getAllGames,
+    getAllConsoles,
+    getAllStudios,
+};
