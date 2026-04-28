c7846cf2b01996ce120aedcf52312bde4aecb80e
SapphicBear <sleepyscreenart@gmail.com>
2026-04-28 21:38:19 +0200

    Refactor DB game code into a class

--- db/queries.js
+++ db/queries.js
@@ -1,15 +1,139 @@
 const pool = require("./pool");
 
-// get all items
-async function getAllGames() {
-    const query = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-    ORDER BY games.id;
-    `
-    const { rows } = await pool.query(query);
-    return rows;
+class Games {
+    async getAllGames() {
+        const query = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+        ORDER BY games.id;
+        `;
+        const { rows } = await pool.query(query);
+        return rows;
+    }
+    async getSelectedGame(game) {
+        const query = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+        WHERE games.id = $1
+        ORDER BY games.id;
+        `;
+        const { rows } = await pool.query(query, [game.id]);
+        return rows;
+    }
+    async filterByConsole(consoleName) {
+        const query = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+            WHERE $1 = ANY(games.console)
+        ORDER BY games.id;
+        `;
+        const queryAlt = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+            WHERE $1 = ANY(games.console) AND in_stock != '0'
+        ORDER BY games.id;
+        `;
+        if (!consoleName.inStock) {
+            const { rows } = await pool.query(query, [consoleName.consoles]);
+            return rows;
+        } else {
+            const { rows } = await pool.query(queryAlt, [consoleName.consoles]);
+            return rows;
+        }
+    }
+    async filterByGenre(genre) {
+        const query = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+            WHERE games.genre = $1
+        ORDER BY games.id;
+        `;
+        const queryAlt = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+            WHERE games.genre = $1 AND in_stock != '0'
+        ORDER BY games.id;
+        `;
+
+        if (!genre.inStock) {
+            const { rows } = await pool.query(query, [genre.genres]);
+            return rows;
+        } else {
+            const { rows } = await pool.query(queryAlt, [genre.genres]);
+            return rows;
+        }
+    }
+    async filterByStudio(studio) {
+        const query = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+            WHERE $1 = game_studios.name
+        ORDER BY games.id;
+        `
+        const queryAlt = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+            WHERE $1 = game_studios.name AND in_stock != '0'
+        ORDER BY games.id;
+        `;
+        if (!studio.inStock) {
+            const { rows } = await pool.query(query, [studio.studios]);
+            return rows;
+        } else {
+            const { rows } = await pool.query(queryAlt, [studio.studios]);
+            return rows;
+        }
+    }
+    async getSelectedGame(game) {
+        const query = `
+        SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console, games.id FROM games
+            JOIN game_studios
+            ON (games.studio_id = game_studios.studio_id)
+        WHERE games.id = '$1'
+        ORDER BY games.id;
+        `;
+        const { rows } = await pool.query(query, [game.id]);
+        return rows;
+    }
+    async updateGameInfo(info) {
+        const query = `
+        UPDATE games 
+        SET name = '$1', genre = '$2', studio_id = (SELECT studio_id FROM game_studios
+                    WHERE game_studios.name = '$3'), release_year = '$4', in_stock = '$5', price = '$6', console = '{{$7}}'
+            WHERE id = $8;
+        `;
+        await pool.query(query, [info.name, info.genre, info.studio, info.release_year, info.in_stock, info.price, info.console, info.id]);
+    }
+    async postNewItem(item) {
+        const query = `
+        INSERT INTO games (name, studio_id, console, genre, release_year, price, in_stock) 
+            VALUES (
+            '$1',
+            (SELECT studio_id FROM game_studios 
+                WHERE name = '$2'),
+            '{{$3}}',
+            '$4',
+            '$5',
+            '$6',
+            '$6'
+            );
+        `;
+        await pool.query(query, [item.game_name, item.studio_name, item.console, item.genre, item.release_year, item.price, item.in_stock]);
+    }
+    async deleteGame(game) {
+        const query = `
+        DELETE FROM games WHERE games.id = '$1';
+        `;
+        await pool.query(query, [game.id]);
+    }
 }
 
 async function getAllConsoles() {
@@ -37,120 +161,6 @@ async function remainingConsoles(consoleName) {
     return rows;
 }
 
-async function filterByConsole(consoleName) {
-    const query = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-        WHERE '${consoleName.consoles}' = ANY(games.console)
-    ORDER BY games.id;
-    `
-    const queryAlt = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-        WHERE '${consoleName.consoles}' = ANY(games.console) AND in_stock != '0'
-    ORDER BY games.id;
-    `;
-    if (!consoleName.inStock) {
-        const { rows } = await pool.query(query);
-        return rows;
-    } else {
-        const { rows } = await pool.query(queryAlt);
-        return rows;
-    }
-    
-}
-async function filterByGenre(genre) {
-    const query = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-        WHERE games.genre = '${genre.genres}'
-    ORDER BY games.id;
-    `;
-    const queryAlt = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-        WHERE games.genre = '${genre.genres}' AND in_stock != '0'
-    ORDER BY games.id;
-    `;
-
-    if (!genre.inStock) {
-        const { rows } = await pool.query(query);
-        return rows;
-    } else {
-        const { rows } = await pool.query(queryAlt);
-        return rows;
-    }
-}
-
-async function filterByStudio(studio) {
-    console.log(studio.studios)
-    const query = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-        WHERE '${studio.studios}' = game_studios.name
-    ORDER BY games.id;
-    `
-    const queryAlt = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console AS console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-        WHERE '${studio.studios}' = game_studios.name AND in_stock != '0'
-    ORDER BY games.id;
-    `;
-    if (!studio.inStock) {
-        const { rows } = await pool.query(query);
-        return rows;
-    } else {
-        const { rows } = await pool.query(queryAlt);
-        return rows;
-    }
-    
-}
-
-async function getSelectedGame(game) {
-    const query = `
-    SELECT games.name AS game_name, game_studios.name AS studio_name, price, in_stock, genre, games.release_year, games.console, games.id FROM games
-        JOIN game_studios
-        ON (games.studio_id = game_studios.studio_id)
-    WHERE games.id = '${game.id}'
-    ORDER BY games.id;
-    `;
-    const { rows } = await pool.query(query);
-    return rows;
-}
-
-async function updateGameInfo(info) {
-    const query = `
-    UPDATE games 
-    SET name = '${info.name}', genre = '${info.genre}', studio_id = (SELECT studio_id FROM game_studios
-                WHERE game_studios.name = '${info.studio}'), release_year = '${info.release_year}', in_stock = '${info.in_stock}', price = '${info.price}', console = '{{${info.console}}}'
-        WHERE id = ${info.id};
-    `;
-    await pool.query(query);
-}
-
-async function postNewItem(item) {
-    const query = `
-    INSERT INTO games (name, studio_id, console, genre, release_year, price, in_stock) 
-        VALUES (
-        '${item.game_name}',
-        (SELECT studio_id FROM game_studios 
-            WHERE name = '${item.studio_name}'),
-        '{{${item.console}}}',
-        '${item.genre}',
-        '${item.release_year}',
-        '${item.price}',
-        '${item.in_stock}'
-        );
-    `;
-    await pool.query(query);
-}
-
 async function postNewStudio(studio) {
     const query = `
     INSERT INTO game_studios (name, year) 
@@ -173,12 +183,6 @@ async function postNewConsole(con) {
     await pool.query(query);
 }
 
-async function deleteGame(game) {
-    const query = `
-    DELETE FROM games WHERE games.id = '$1';
-    `;
-    await pool.query(query, [game.id]);
-}
 async function deleteConsole(con) {
     const query = `
     DELETE FROM game_consoles WHERE console_id = '$1';
@@ -194,20 +198,16 @@ async function deleteStudio(studio) {
 }
 
 module.exports = {
-    getAllGames,
+    Games,
     getAllConsoles,
     getAllStudios,
     getAllGenres,
-    filterByConsole,
-    filterByGenre,
-    getSelectedGame,
-    updateGameInfo,
+   
     remainingConsoles,
-    postNewItem,
-    filterByStudio,
+
     postNewStudio,
     postNewConsole,
-    deleteGame,
+
     deleteConsole,
     deleteStudio
 };
